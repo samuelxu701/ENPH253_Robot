6d35aef31be2a747a72560604c2e68285c6ead6d
Andrew Xie <andrewxieax1@gmail.com>
2021-07-22 22:03:33 -0700

    Created canPickup branch

--- src/main.cpp
+++ src/main.cpp
@@ -26,53 +26,41 @@ void setup(){
 }
 
 void loop(){
-    checkIRreceiver();
+    display.clearDisplay();
+    display.setCursor(0,0);
 
-    if(receivingIRData){
-        driveMotors(0,0,0,0);
-        parameterMenuLoop();
-    }else{
-        tapeFollowingLoop();
+    long distance = readSonar();
+
+    display.println(distance);
+    display.print("Is Can Detected? ");
+    display.println((isCanDetected) ? "True" : "False");
+    display.print("Can Count: ");
+    display.println(canCount);
+    // display.print("Docking Status: ");
+    // display.println(dockingStatus);
+    display.display();
+
+    if (robotState == driving) {
+        // switch (dockingStatus) {
+        //     case 0:
+        //         // changeSpeed(/* Regular */);
+        //         break;
+        //     case 1:
+        //         // changeSpeed(/* Slower */);
+        //         break;
+        //     case 2:
+        //         // changeSpeed(/* Stop */);
+        //         dropoffBump();
+        //         canCount--;
+        //         break;
+        // }
+        if (isCanDetected) {
+            driveMotors(0, 0, 0, 0);
+            // display.clearDisplay();
+            // display.setCursor(0,0);
+            // display.println("Can Detected");
+            // display.display();
+            canPickup();
+        }
     }
-
-
-
-    // tapeFollowingLoop();
-    // display.clearDisplay();
-    // display.setCursor(0,0);
-
-    // long distance = readSonar();
-
-    // // display.println(distance);
-    // // display.print("Is Can Detected? ");
-    // // display.println((isCanDetected) ? "True" : "False");
-    // // display.print("Can Count: ");
-    // // display.println(canCount);
-    // // display.print("Docking Status: ");
-    // // display.println(dockingStatus);
-    // // display.display();
-
-    // if (robotState == driving) {
-    //     // switch (dockingStatus) {
-    //     //     case 0:
-    //     //         // changeSpeed(/* Regular */);
-    //     //         break;
-    //     //     case 1:
-    //     //         // changeSpeed(/* Slower */);
-    //     //         break;
-    //     //     case 2:
-    //     //         // changeSpeed(/* Stop */);
-    //     //         dropoffBump();
-    //     //         canCount--;
-    //     //         break;
-    //     // }
-    //     if (isCanDetected) {
-    //         driveMotors(0, 0, 0, 0);
-    //         display.clearDisplay();
-    //         display.setCursor(0,0);
-    //         display.println("Can Detected");
-    //         display.display();
-    //         canPickup();
-    //     }
-    // }
 }
