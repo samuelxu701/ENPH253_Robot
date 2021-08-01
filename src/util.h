195cb0ccd41f8d49548026292cb138035ae975b9
yel-wishahy <yel.wishahy@gmail.com>
2021-08-01 14:53:07 -0700

    --Latest Tested Code-- *removed unused util methods

--- src/util.h
+++ src/util.h
@@ -1,8 +1,6 @@
 #ifndef ROBOTUTIL_H
 #define ROBOTUTIL_H
 
-extern volatile bool onTape;
-
 //Hardware Timers
 // extern HardwareTimer tim1;
 // extern HardwareTimer tim2;
@@ -12,12 +10,6 @@ extern volatile bool onTape;
 
 //Takes an analog value and reduces it to a binary based on a threshold value
 int binaryProcessor(int reading, int threshold);
-
-//Converts stm32 analog values to a voltage based on a 3.3/1024 unit scale
-float analogReadToVoltage(int bitAnalogValue);
-
-//prototype interrupt function still being tested
-void handleTapeSensorInterrupt();
 
 //Display timer value on adafuit display. Value type depends on format
 //HERTZ_FORMAT = display frequency
