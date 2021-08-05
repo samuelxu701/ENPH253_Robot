1df795981aaf325c4117ee5fa58105eff8f38273
Samuel Xu <samuel_xu@Samuels-MacBook-Pro.local>
2021-08-04 18:43:55 -0700

    slight changes to pid

--- src/candropoff.cpp
+++ src/candropoff.cpp
@@ -82,16 +82,16 @@ void canDropoff(){
             // driveMotors(0,0,0,0);
             // delay(motorStopDelay);
 
-            //reverse tape follow until docking sensor on tape again
-            // while(analogRead(DOCKING_SENSOR) < binaryThreshold){
-            //     tapeFollowingPID(1, max_pwm, false);
-            // }
+            // reverse tape follow until docking sensor on tape again
+            while(analogRead(DOCKING_SENSOR) < binaryThreshold){
+                tapeFollowingPID(1, max_pwm, false);
+            }
 
             //blind reverse
-            if(analogRead(DOCKING_SENSOR) < binaryThreshold){
-                driveMotors(0, max_pwm, 0, max_pwm);
-            }
-            while(analogRead(DOCKING_SENSOR) < binaryThreshold);    
+            // if(analogRead(DOCKING_SENSOR) < binaryThreshold){
+            //     driveMotors(0, max_pwm, 0, max_pwm);
+            // }
+            // while(analogRead(DOCKING_SENSOR) < binaryThreshold);    
                 
 
             //stop after reverse
--- src/tapefollowing.cpp
+++ src/tapefollowing.cpp
@@ -26,12 +26,12 @@ Set 1: 19.5s
 
 //*********TAPE FOLLOWING PID PARAMETERS********//
 int kp = 25;
-int kd = 60;
+int kd = 70;
 int binaryThreshold = 650;
 
 //***********SPEED/TURNING PARAMETERS********//
-int max_pwm = 1050;//1100;// 1125;
-double multiplier = 20;
+int max_pwm = 1125;
+double multiplier = 27;
 int absolute_maximum_pwm = 4096;
 double outMult = 1;
 double inMult = 1;
